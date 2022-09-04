02a2b0908a99abcb5ff1c0f994963ad46ee257f9
Pavan Sargar <pavansargar36@gmail.com>
2022-09-04 11:23:15 +0530

    fixed error

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -7,19 +7,9 @@ import FALLLBACKIMG from "../../assets/homeImages/Loading....png";
 
 // import Slider from "react-slick";
 
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
 import styles from "./Portfolio.module.css";
 
 function Portfolio(props) {
-  const settings = {
-    dots: false,
-    lazyLoad: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-  };
   return (
     <Container
       data-aos={props.animation}
@@ -29,13 +19,6 @@ function Portfolio(props) {
       <h2>Portfolio</h2>
       <hr />
       <Container className={styles["slider-container"]}>
-        {/* <Slider {...settings}>
-          {images.map((img, i) => (
-            <div key={i} className={styles["slider-images"]}>
-              <img alt="mayur tade portfolio images" src={img.imgURL} />
-            </div>
-          ))}
-        </Slider> */}
         {images.map((img, i) => (
           <LazyLoadImage
             key={i + Date.now()}
